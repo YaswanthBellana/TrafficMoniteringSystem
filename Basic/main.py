b11d29aa19c2a61b17a23ca81fd925be6b1a6db0
BELLANA YASWANTH <139348041+YaswanthBellana@users.noreply.github.com>
2025-01-17 15:18:55 +0530

    Update main.py

--- Basic/main.py
+++ Basic/main.py
@@ -1,1 +1,23 @@
+import cv2
+import numpy as np
+cap = cv2.VideoCapture('video.mp4')
 
+bg_subtractor = cv2.createBackgroundSubtractorMOG2()
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    fg_mask = bg_subtractor.apply(frame)
+    thresh = cv2.threshold(fg_mask, 254, 255, cv2.THRESH_BINARY)[1]
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('Frame', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('z'):
+        break
+cap.release()
+cv2.destroyAllWindows()
